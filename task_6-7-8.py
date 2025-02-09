d91fa8e20b4129a328644cb97db406231dbf778a
Dima Laptop <mityaimorozov@gmail.com>
2025-02-09 18:27:33 +0300

    Доделанные задания 6, 7, 8 лаба 1

--- task_6-7-8.py
+++ task_6-7-8.py
@@ -52,4 +52,25 @@ def solve_task_8():
             current_count = 0
     print("Наибольшее количество подряд идущих цифр:", max_count)
 
-solve_task_8()
+def solve_task_6_7_8():
+    while True:
+        print("\nВыберите задачу для решения:")
+        print("6 - Найти наибольшее количество подряд идущих символов кириллицы")
+        print("7 - Найти минимальное натуральное число в строке")
+        print("8 - Найти наибольшее количество подряд идущих цифр")
+        print("0 - Выход")
+        choice = input("Ваш выбор: ")
+
+        if choice == "6":
+            solve_task_6()
+        elif choice == "7":
+            solve_task_7()
+        elif choice == "8":
+            solve_task_8()
+        elif choice == "0":
+            print("Выход из программы.")
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, выберите 6, 7, 8 или 0 для выхода.")
+
+solve_task_6_7_8()
