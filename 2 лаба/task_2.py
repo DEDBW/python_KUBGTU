b4006d0abc96f62554f94e63204bf64f8e7ee642
Dima Laptop <mityaimorozov@gmail.com>
2025-02-15 15:15:14 +0300

    Задание 2 лаба 2

---
+++ 2 лаба/task_2.py
@@ -0,0 +1,15 @@
+text = input().strip()
+words = text.split()
+
+word_count = {}
+result = []
+
+for word in words:
+    if word in word_count:
+        result.append(word_count[word])
+        word_count[word] += 1
+    else:
+        result.append(0)
+        word_count[word] = 1
+
+print(" ".join(map(str, result)))
